629d57b6e5b66b2c6c9c88305135206e047d806b
Edward Yoo <ehyoo97@gmail.com>
2016-09-16 19:47:07 -0400

    flexible message sending

--- hamr_transcriber.py
+++ hamr_transcriber.py
@@ -6,37 +6,74 @@ No more ROS as core communication.
 """
 
 class HamrTranscriber():
-    def __init__(self, port=None):
+    def __init__(self, port=None, msg_type=None, baud_rate=57600):
         self.val_map = {'tm': 'timestamp',
             'lv': 'left_motor_velocity',
             'rv': 'right_motor_velocity',
             'tv': 'turret_motor_velocity',
             'dl': 'desired_left_velocity',
             'dr': 'desired_right_velocity',
-            'dt': 'desired_turret_velocity'
+            'dt': 'desired_turret_velocity',
+            'cx': 'computed_xdot',
+            'cy': 'computed_ydot',
+            'ct': 'computed_tdot',
+            'hx': 'set_xdot',
+            'hy': 'set_ydot',
+            'hr': 'set_rdot',
+            'sd': 'sensed_drive_angle'
         }
         # MESSAGE DEFINITIONS:
-        # velocity_message: contains the timestamp, velocities and desired
-        # velocities of the HAMR. 
+        self.raw_velocity = set(['tm', 'lv', 'rv', 'tv', 'dl', 'dr', 'dt'])
+        self.hamr_state = set(['cx', 'cy', 'ct', 'hx', 'hy', 'hr', 'sd'])
         if port == None:
             raise AttributeError('please define the port')
-        self.ser = serial.Serial(port, 57600)
+        self.ser = serial.Serial(port, baud_rate)
+        if msg_type == None:
+            raise AttributeError(
+                'please define the message type you wish to read')
+        self.msg_type = msg_type
 
-    def _read(self):
+    def _read_ser(self):
         """
-        Returns the message sent from the HAMR in the form of a dictionary
-        Values are raw strings.
+        Read from serial and return raw string of data
         """
         message = {}
         self.ser.close()
-        self.ser.open() # do something about this later.
-        raw_string = self.ser.readline()
-        self.ser.close() # instead of handling it like this
-        raw_data_list = raw_string.split(' ')
+        self.ser.open()
+        raw_data_list = self.ser.readline().split(' ')
         for i in raw_data_list:
             line_tuple = self._parse_result(i)
             message[line_tuple[0]] = line_tuple[1]
+        message_attr = getattr(self, self.msg_type)
+        if set(message.keys()) != message_attr:
+            message = self._read_ser()
         return message
+
+    def read(self, msg_format='default'):
+        """
+        Returns the message sent from the HAMR
+        default: python dictionary of {string : float} format
+        """
+        raw_message = self._read_ser()
+        if msg_format == 'default':
+            return self._default_formatting(raw_message)
+        elif msg_format == 'ros':
+            print 'do the ros'
+
+    def _default_formatting(self, raw_dict):
+        """
+        Returns string: float format dictionary
+        """
+        formatted_msg = {}
+        for key, val in raw_dict.iteritems():
+            formatted_msg[self.val_map[key]] = float(str.strip(val))/1000.0
+        return formatted_msg
+
+    def stop_read(self):
+        """
+        Close serial and finish reading from the HAMR
+        """
+        self.ser.close()
 
     def _parse_result(self, line):
         """
@@ -45,20 +82,9 @@ class HamrTranscriber():
         """
         return (line[:2], line[2:])
 
-    def jsonify(self):
-        print "Not yet implemented"
-
-
-
-"""
-We'd want to export this in several ways: 
-
-default python hash
-json object
-ROS message
-"""
 
 if __name__ == '__main__':
-    ht = HamrTranscriber('/dev/ttyACM0')
-    while True:
-        print ht._read()
+    ht = HamrTranscriber('/dev/ttyACM0', 'hamr_state')
+    for i in range(0,100):
+        print ht.read()
+    ht.stop_read()
