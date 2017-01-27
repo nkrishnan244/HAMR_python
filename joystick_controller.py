fb3e424b8222e6b8aabc9e17185429fac3f8cfe1
Edward Yoo <ehyoo97@gmail.com>
2017-01-27 16:12:04 -0500

    adds stopping toggle with joystick

--- joystick_controller.py
+++ joystick_controller.py
@@ -7,7 +7,7 @@ class JoystickController:
 
     It is important to note that this joystick uses a very particular kind of joystick.
     Usage Instructions:
-        The Joystick Controller starts in holonomic mode. 
+        The Joystick Controller starts in holonomic mode.
         You can toggle between modes by pressing button 9.
         In holonomic mode:
             Use left joystick to orient the turret
@@ -18,7 +18,7 @@ class JoystickController:
             Use the left/right trigger buttons to move the turret.
             Use buttons 1 and 4 to decrease/increase the speed at which the turret moves, respectively.
 
-        It's important to note that you have total control of the speed of the motors in holonomic mode, but 
+        It's important to note that you have total control of the speed of the motors in holonomic mode, but
         have to manually adjust the speed at which the turret moves in dif drive mode.
 
     Attributes:
@@ -44,8 +44,8 @@ class JoystickController:
             'INCREASE_SCALAR': 3,
             'LEFT_TRIGGER': 6,
             'RIGHT_TRIGGER': 7,
-            'TOGGLE_MODE': 9,
-            'KILL_MOTORS': 10
+            'TOGGLE_MODE': 8,
+            'KILL_MOTORS': 9
         }
         self.holonomic_mode = True
         pygame.init()
@@ -66,10 +66,17 @@ class JoystickController:
             if (event.button == self.button_ids['KILL_MOTORS']):
                 print 'motors should be killed'
             elif (event.button == self.button_ids['TOGGLE_MODE']):
-                self.holonomic_mode = !self.holonomic_mode
-                print 'mode toggled'
-            elif (!self.holonomic_mode):
+                self.holonomic_mode = not self.holonomic_mode
+                if (self.holonomic_mode):
+                    print 'Toggling Holonomic Mode.'
+                else:
+                    print 'Toggling DifDrive Mode.'
+            elif (not self.holonomic_mode):
                 self.handle_dif_drive_button_event(event)
+        elif (event.type == self.event_types['BUTTON_UP']):
+            if (event.button == self.button_ids['LEFT_TRIGGER']) or (event.button == self.button_ids['RIGHT_TRIGGER']):
+                print 'stopping the turret'
+
 
     def handle_holonomic_joystick_event(self, axis, value):
         if (axis == self.joystick_axes['X_LEFT']):
@@ -101,7 +108,7 @@ if __name__ == '__main__':
         event_list = event.get()
         for event_obj in event_list:
             jc.handle_event(event_obj)
-        
+
     # for event_obj in event_list:
     #     if event_obj.type == 7:
     #         joystick_direction = event_obj.axis
